812dcfd2d4efc3e917a3517ebf0a3b77d29d8d65
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-02-19 19:19:43 +0100

    added rbfKernel to transductive

--- cmd/transductive/transductive.go
+++ cmd/transductive/transductive.go
@@ -1,9 +1,16 @@
 package transductive
 
+import "math"
+
 func Transductive() {
 
 }
 
-func Kernel() {
+func rbfKernel(x1 [2]float64, x2 [2]float64, variance float64) float64 {
+	value := -math.Pow(x1[0]-x2[0], 2) + math.Pow(x1[1]-x2[1], 2)
+	value = math.Sqrt(value)
+	value /= 2 * variance
+	value = math.Exp(value)
 
+	return value
 }
