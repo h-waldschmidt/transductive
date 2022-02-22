27d5b52bbdc17844e2fef92067a5b127196c0a7e
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-02-22 21:08:00 +0100

    started implementing sequential algorithm

--- cmd/transductive/sequential.go
+++ cmd/transductive/sequential.go
@@ -1,10 +1,31 @@
 package transductive
 
-func sequentialOptimization(points []Coordinate) {
-	//select x to maximize function
+import "math"
 
-	// add it to set
+func sequentialOptimization(points []Coordinate, numOfPoints int, mean float64) {
+	var selectedPoints []Coordinate
 
-	// normalize the Kvv function
+	// find out how to calculate the variance
+	kVVMatrix := calculateKernelMatrix(points, points, 0)
 
+	for len(selectedPoints) < numOfPoints {
+		//select x to maximize function
+		var currentX Coordinate
+		bestValue := math.Inf(1)
+
+		for i := 0; i < len(points); i++ {
+			currentX = points[i]
+
+		}
+		// add it to set
+
+		// normalize the Kvv function
+	}
+
+}
+
+func calculateCriteria(points []Coordinate, currentX Coordinate, variance float64, mean float64) float64 {
+	kVxVector := calculateKernelVector(points, currentX, variance)
+	value := math.Pow(euclideanNorm(kVxVector), 2) / (rbfKernel(currentX, currentX, variance) + mean)
+	return value
 }
