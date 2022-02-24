74de9126b01580c344d7e63f7a554678d4361c38
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-02-24 20:30:33 +0100

    continuing sequential

--- cmd/transductive/sequential.go
+++ cmd/transductive/sequential.go
@@ -6,21 +6,26 @@ func sequentialOptimization(points []Coordinate, numOfPoints int, mean float64) 
 	var selectedPoints []Coordinate
 
 	//TODO: find out how to calculate the variance
-
+	var variance float64
 	//initialize the kVVMatrix
 	kVVMatrix := calculateKernelMatrix(points, points, 0)
 
 	for len(selectedPoints) < numOfPoints {
 		//select x to maximize the criteria
-		var currentX Coordinate
+		var bestX Coordinate
 		bestValue := math.Inf(1)
 
 		for i := 0; i < len(points); i++ {
-			currentX = points[i]
-
+			currentX := points[i]
+			currentValue := calculateCriteria(points, currentX, variance, mean)
+			if currentValue < bestValue {
+				bestValue = currentValue
+				bestX = currentX
+			}
 		}
-		// add it the newly found x to the set
 
+		// add it the newly found x to the set
+		selectedPoints = append(selectedPoints, bestX)
 		// normalize the Kvv function by removing the influence of x
 	}
 
@@ -33,3 +38,5 @@ func calculateCriteria(points []Coordinate, currentX Coordinate, variance float6
 	value := math.Pow(euclideanNorm(kVxVector), 2) / (rbfKernel(currentX, currentX, variance) + mean)
 	return value
 }
+
+func normalizeKvvMatrix(kVVMatrix Matrix) Matrix {}
