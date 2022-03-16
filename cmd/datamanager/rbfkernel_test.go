65a819742a09712ffe0bf550deb0fef2f26a80c2
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-03-16 16:26:58 +0100

    added basic test for rbfkernel

---
+++ cmd/datamanager/rbfkernel_test.go
@@ -0,0 +1,17 @@
+package datamanager
+
+import (
+	"math"
+	"testing"
+)
+
+func TestRBFKernel(t *testing.T) {
+	x := Coordinate{X1: 1, X2: 1}
+	y := Coordinate{X1: 2, X2: 2}
+
+	value := RbfKernel(x, y, 1)
+	expected := math.Exp(-1)
+	if value != expected {
+		t.Errorf("Expected: %f ; Got: %f", expected, value)
+	}
+}
