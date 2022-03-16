79fe7531718d682d0152c050cd9c6e36d7f5dcdc
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-03-16 16:26:47 +0100

    fixed bug in rbfkernel

--- cmd/datamanager/rbfkernel.go
+++ cmd/datamanager/rbfkernel.go
@@ -7,10 +7,9 @@ import (
 // using the popular rbfKernel (https://en.wikipedia.org/wiki/Radial_basis_function_kernel)
 // is necessary for the kernel regression
 func RbfKernel(x1 Coordinate, x2 Coordinate, sigma float64) float64 {
-	value := -math.Pow(x1.X1-x2.X1, 2) + math.Pow(x1.X2-x2.X2, 2)
-	value = math.Sqrt(value)
+	value := math.Pow(x1.X1-x2.X1, 2) + math.Pow(x1.X2-x2.X2, 2)
 	value /= 2 * sigma
-	value = math.Exp(value)
+	value = math.Exp(-value)
 
 	return value
 }
