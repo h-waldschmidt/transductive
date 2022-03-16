e24fba196b8e6a4d32350ed90d9e74fc253f80e3
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-03-16 17:44:24 +0100

    used wrong package

--- cmd/datamanager/rbfkernel_test.go
+++ cmd/datamanager/rbfkernel_test.go
@@ -1,16 +1,18 @@
-package datamanager
+package datamanager_test
 
 import (
 	"math"
 	"testing"
+	"transductive-experimental-design/cmd/datamanager"
 )
 
 func TestRBFKernel_Basic(t *testing.T) {
-	x := Coordinate{X1: 1, X2: 1}
-	y := Coordinate{X1: 2, X2: 2}
+	x := datamanager.Coordinate{X1: 1, X2: 1}
+	y := datamanager.Coordinate{X1: 2, X2: 2}
 
-	value := RbfKernel(x, y, 1)
+	value := datamanager.RbfKernel(x, y, 1)
 	expected := math.Exp(-1)
+
 	if value != expected {
 		t.Errorf("Expected: %f ; Got: %f", expected, value)
 	}
