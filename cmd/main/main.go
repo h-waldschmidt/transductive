9bc75a6bb7f12c29191b9ecf6e6532394e284f24
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-03-17 09:48:03 +0100

    added testing of sequential in main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"transductive-experimental-design/cmd/datamanager"
+	"transductive-experimental-design/cmd/transductive"
 )
 
 func main() {
 	distribution := datamanager.CreateNormalDistribution(0, 1, 50)
+	anotherDistribution := datamanager.CreateNormalDistribution(4, 1, 50)
+	distribution = append(distribution, anotherDistribution...)
 
-	datamanager.PlotDistribution(distribution, "../../plots/test.png")
+	test := transductive.SequentialOptimization(distribution, 2, 1, 1)
+	datamanager.PlotSelectedPoints(distribution, test, "../../plots/test_sequential.png")
 }
