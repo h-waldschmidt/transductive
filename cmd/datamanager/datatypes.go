52803347431b44f1d12f2b987269b814bbf29205
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-03-26 13:38:25 +0100

    documentation datatypes

--- cmd/datamanager/datatypes.go
+++ cmd/datamanager/datatypes.go
@@ -3,12 +3,11 @@ package datamanager
 // TODO: Change everything to support Vector instead of Coordinate
 type Coordinate struct{ X1, X2 float64 }
 
+/**
+ * N represents the number of columns (so that a vector can be in one array)
+ * M represents the number rows (e.g. dimension of a vector)
+ */
 type Matrix struct {
 	N, M   int
 	Matrix [][]float64
-}
-
-type Vector struct {
-	N      int
-	Vector []float64
 }
