5c0c69d77495393c6d00b2f3764e269d9f82c1fc
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-03-27 16:12:27 +0200

    tested sequential (is functioning)

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,10 +6,15 @@ import (
 )
 
 func main() {
-	distribution := datamanager.CreateNormalDistribution(0, 0.1, 2)
-	anotherDistribution := datamanager.CreateNormalDistribution(4, 0.1, 2)
+	distribution := datamanager.CreateNormalDistribution(0, 0.1, 10)
+	anotherDistribution := datamanager.CreateNormalDistribution(4, 0.1, 10)
 	distribution = append(distribution, anotherDistribution...)
+	another := datamanager.CreateNormalDistribution(8, 0.1, 10)
 
-	test := transductive.SequentialOptimization(distribution, 2, 1.8, 1.5)
-	datamanager.PlotSelectedPoints(distribution, test, "../../plots/test_sequential.png")
+	distribution = append(distribution, another...)
+	matrix := datamanager.ConvertCoordinatesToMatrix(distribution)
+
+	test := transductive.SequentialOptimization(matrix, 3, 1.8, 1.5)
+	distribution_test, _ := datamanager.ConvertMatrixToCoordinateSlice(test)
+	datamanager.PlotSelectedPoints(distribution, distribution_test, "../../plots/test_sequential.png")
 }
