187fcbb892cb9f832fbd9699692fa007e3d74431
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-03-27 13:46:25 +0200

    sequential now works with new paramaters

--- cmd/transductive/sequential.go
+++ cmd/transductive/sequential.go
@@ -5,19 +5,21 @@ import (
 	"transductive-experimental-design/cmd/datamanager"
 )
 
-func SequentialOptimization(points []datamanager.Coordinate, numOfSelectedPoints int, lambda float64, sigma float64) []datamanager.Coordinate {
-	var selectedPoints []datamanager.Coordinate
-
+func SequentialOptimization(points datamanager.Matrix, numOfSelectedPoints int, lambda float64, sigma float64) datamanager.Matrix {
+	selectedPoints := datamanager.Matrix{numOfSelectedPoints, points.M, make([][]float64, numOfSelectedPoints)}
+	for i := 0; i < selectedPoints.N; i++ {
+		selectedPoints.Matrix[i] = make([]float64, selectedPoints.M)
+	}
 	//initialize the kVVMatrix
-	kVVMatrix := datamanager.CalculateKernelMatrix(points, points, sigma)
+	kVVMatrix, _ := datamanager.CalculateKernelMatrix(points, points, sigma)
 
-	for len(selectedPoints) < numOfSelectedPoints {
+	for j := 0; j < numOfSelectedPoints; j++ {
 		//select x to maximize the criteria
-		var bestX datamanager.Coordinate
+		var bestX []float64
 		bestValue := math.Inf(-1)
 
-		for i := 0; i < len(points); i++ {
-			currentX := points[i]
+		for i := 0; i < points.M; i++ {
+			currentX := points.Matrix[i]
 			currentValue := calculateCriteria(kVVMatrix, currentX, i, sigma, lambda)
 			if currentValue > bestValue {
 				bestValue = currentValue
@@ -26,7 +28,7 @@ func SequentialOptimization(points []datamanager.Coordinate, numOfSelectedPoints
 		}
 
 		// add it the newly found x to the set
-		selectedPoints = append(selectedPoints, bestX)
+		selectedPoints.Matrix[j] = bestX
 		// normalize the Kvv function by removing the influence of x
 		kVVMatrix = normalizeKvvMatrix(kVVMatrix, points, bestX, sigma, lambda)
 	}
@@ -35,7 +37,7 @@ func SequentialOptimization(points []datamanager.Coordinate, numOfSelectedPoints
 
 // basically calculates the distance from all points to the given point
 // and takes the datamanager.euclideanNorm of the resulting vector
-func calculateCriteria(kVVMatrix datamanager.Matrix, currentX datamanager.Coordinate, index int, sigma float64, lambda float64) float64 {
+func calculateCriteria(kVVMatrix datamanager.Matrix, currentX []float64, index int, sigma float64, lambda float64) float64 {
 	kVxVector := datamanager.Matrix{kVVMatrix.N, 1, make([][]float64, kVVMatrix.N)}
 	for i := 0; i < kVxVector.N; i++ {
 		kVxVector.Matrix[i] = []float64{kVVMatrix.Matrix[index][i]}
@@ -44,16 +46,16 @@ func calculateCriteria(kVVMatrix datamanager.Matrix, currentX datamanager.Coordi
 	kxVVector := datamanager.TransposeMatrix(kVxVector)
 	//value, _ := datamanager.EuclideanNorm(kVxVector)
 	value, _ := datamanager.MatrixMultiplication(kxVVector, kVxVector)
-	result := value.Matrix[0][0] / (datamanager.RbfKernel(currentX, currentX, sigma) + lambda)
+	result := value.Matrix[0][0] / (1 + lambda)
 	return result
 }
 
-func normalizeKvvMatrix(kVVMatrix datamanager.Matrix, points []datamanager.Coordinate, point datamanager.Coordinate, lambda float64, sigma float64) datamanager.Matrix {
-	VxMatrix := datamanager.CalculateKernelVector(points, point, sigma)
+func normalizeKvvMatrix(kVVMatrix datamanager.Matrix, points datamanager.Matrix, point []float64, lambda float64, sigma float64) datamanager.Matrix {
+	VxMatrix, _ := datamanager.CalculateKernelVector(points, point, sigma)
 	xVMatrix := datamanager.TransposeMatrix(VxMatrix)
 	VxxVMatrix, _ := datamanager.MatrixMultiplication(VxMatrix, xVMatrix)
 
-	VxxVMatrix = datamanager.MatrixScalarMultiplication(VxxVMatrix, 1/(datamanager.RbfKernel(point, point, sigma)+lambda))
+	VxxVMatrix = datamanager.MatrixScalarMultiplication(VxxVMatrix, 1/(1+lambda))
 	kVVMatrix, _ = datamanager.MatrixSubtraction(kVVMatrix, VxxVMatrix)
 
 	return kVVMatrix
