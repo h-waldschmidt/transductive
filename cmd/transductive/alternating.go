5f9d9a67c814cabaa8743f29a4a75fdd927e7f18
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-04-10 14:17:48 +0200

    added fist part of the alternating algorithm

--- cmd/transductive/alternating.go
+++ cmd/transductive/alternating.go
@@ -1,10 +1,38 @@
 package transductive
 
 import (
+	"log"
 	"transductive-experimental-design/cmd/datamanager"
 )
 
 func AlternatingOptimization(points datamanager.Matrix, numOfSelectedPoints int, lambda float64, sigma float64) datamanager.Matrix {
+	points_T := points.TransposeMatrix()
+	k, err := datamanager.MatrixMultiplication(points, points_T)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	eigen, err := k.CalculateEigen()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	kk, err := datamanager.MatrixMultiplication(k, k)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	kk_slice := make([]datamanager.Matrix, len(eigen.Values))
+	for i := 0; i < len(eigen.Values); i++ {
+		kk_slice[i] = kk
+		for j := 0; j < kk_slice[i].M; j++ {
+			kk_slice[i].Matrix[j][j] += lambda * eigen.Values[i]
+		}
+		kk_slice[i], err = kk_slice[i].Inverse()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 	//repeat until no major improvement
 	// find optimal alpha
 	// find optimal beta
