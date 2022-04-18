ffdac2305aaa63c09bc60a15d39c803840904346
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-04-18 14:57:54 +0200

    added documentation in alternating

--- cmd/transductive/alternating.go
+++ cmd/transductive/alternating.go
@@ -6,22 +6,28 @@ import (
 )
 
 func AlternatingOptimization(points datamanager.Matrix, numOfSelectedPoints int, lambda float64, sigma float64) datamanager.Matrix {
+
+	// create K = V * V^T matrix with V being the point Matrix
 	points_T := points.TransposeMatrix()
 	k, err := datamanager.MatrixMultiplication(points, points_T)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// eigen components of K matrix
 	eigen, err := k.CalculateEigen()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// create K*K matrix
 	kk, err := datamanager.MatrixMultiplication(k, k)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// create all the (K*K + lambda* eigen_value_i)^-1 matrices
+	// those are needed to find all the alpha_i
 	kk_slice := make([]datamanager.Matrix, len(eigen.Values))
 	for i := 0; i < len(eigen.Values); i++ {
 		kk_slice[i] = kk
@@ -34,6 +40,7 @@ func AlternatingOptimization(points datamanager.Matrix, numOfSelectedPoints int,
 		}
 	}
 	//repeat until no major improvement
+	// for testing purposes I'm running the algorithm a fixed time
 	// find optimal alpha
 	// find optimal beta
 	// normalize Beta Matrix
