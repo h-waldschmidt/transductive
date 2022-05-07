9feab00861195a725ed75ae4b03772ff8a1a0267
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-05-07 18:47:09 +0200

    outline qpsolver

---
+++ cmd/qpsolver/qpsolver.go
@@ -0,0 +1,6 @@
+package qpsolver
+
+//Outline:
+// Since it is pretty hard to write a quadratic-program-solver from scratch,
+// it is easier to call the function in MatLab compile this code to C-Code
+// and then to call this C-Code in go
