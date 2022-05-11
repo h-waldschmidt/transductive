0981f8b02d349233b817056013a302e7f39dfeb9
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-05-11 14:48:53 +0200

    conversion functions for lalg

--- cmd/qpsolver/qpsolver.go
+++ cmd/qpsolver/qpsolver.go
@@ -31,20 +31,49 @@ func SolveQP(D datamanager.Matrix, d datamanager.Matrix, A1 datamanager.Matrix, 
 
 // convert datamanager.Matrix tpye to lalg.Matrix type
 func convertDMatrixToLMatrix(dMatrix datamanager.Matrix) lalg.Matrix {
+	lMatrix := lalg.NewMatrix(dMatrix.M, dMatrix.N)
 
+	for i := 0; i < lMatrix.Rows; i++ {
+		for j := 0; j < lMatrix.Cols; j++ {
+			lMatrix.Set(i, j, dMatrix.Matrix[j][i])
+		}
+	}
+
+	return lMatrix
 }
 
 // convert datamanager.Matrix (with N=1) to lalg.Vector type
 func convertDMatrixToLVector(dVector datamanager.Matrix) lalg.Vector {
+	if dVector.N != 1 {
+		log.Fatal("N of dVector has to be 1")
+	}
 
+	lVector := lalg.NewVector(dVector.M)
+
+	for i := 0; i < len(lVector); i++ {
+		lVector[i] = dVector.Matrix[1][i]
+	}
+
+	return lVector
 }
 
 // convert lalg.Matrix type to datamanager.Matrix type
 func convertLMatrixToDMatrix(lMatrix lalg.Matrix) datamanager.Matrix {
+	dMatrix := datamanager.NewMatrix(lMatrix.Cols, lMatrix.Rows)
 
+	for i := 0; i < dMatrix.N; i++ {
+		for j := 0; j < dMatrix.M; j++ {
+			dMatrix.Matrix[i][j] = lMatrix.Get(j, i)
+		}
+	}
+
+	return *dMatrix
 }
 
 // convert lalg.Vector type to datamanager.Matrix (with N=1) type
 func convertLVectorToDMatrix(lVector lalg.Vector) datamanager.Matrix {
+	dVector := datamanager.NewMatrix(1, len(lVector))
+	dVector.Matrix[1] = lVector
 
+	return *dVector
 }
