7e6fb50e38397f2bed6fe96b456e06f3144528b3
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-06-13 14:42:40 +0200

    plot and point generation in separate class

---
+++ cmd/plt/plot.go
@@ -0,0 +1,100 @@
+package plt
+
+import (
+	"fmt"
+	"image/color"
+	"os"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg/draw"
+)
+
+func PlotDistribution(items []Coordinate, path string) error {
+	var itemsXYs plotter.XYs
+	for _, xy := range items {
+		itemsXYs = append(itemsXYs, struct{ X, Y float64 }{xy.X1, xy.X2})
+	}
+	f, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("could not create %s: %v", path, err)
+	}
+
+	p := plot.New()
+
+	// Add the items as an scatter plot
+	s, err := plotter.NewScatter(itemsXYs)
+	if err != nil {
+		return fmt.Errorf("could not create scatter: %v", err)
+	}
+	s.GlyphStyle.Shape = draw.CrossGlyph{}
+	s.Color = color.RGBA{R: 255, A: 255}
+	p.Add(s)
+
+	wt, err := p.WriterTo(256, 256, "png")
+	if err != nil {
+		return fmt.Errorf("could not create writer: %v", err)
+	}
+	_, err = wt.WriteTo(f)
+	if err != nil {
+		return fmt.Errorf("could not write to %s: %v", path, err)
+	}
+
+	if err := f.Close(); err != nil {
+		return fmt.Errorf("could not close %s: %v", path, err)
+	}
+	return nil
+}
+
+func PlotSelectedPoints(items, selectedPoints []Coordinate, path string) error {
+	var itemsXYs plotter.XYs
+	for _, xy := range items {
+		itemsXYs = append(itemsXYs, struct{ X, Y float64 }{xy.X1, xy.X2})
+	}
+
+	var points plotter.XYs
+	for _, xy := range selectedPoints {
+		points = append(points, struct{ X, Y float64 }{xy.X1, xy.X2})
+	}
+
+	f, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("could not create %s: %v", path, err)
+	}
+
+	p := plot.New()
+
+	// Add the items as an scatter plot
+	s, err := plotter.NewScatter(itemsXYs)
+	if err != nil {
+		return fmt.Errorf("could not create scatter: %v", err)
+	}
+
+	// Add the items as an scatter plot
+	po, err := plotter.NewScatter(points)
+	if err != nil {
+		return fmt.Errorf("could not create scatter: %v", err)
+	}
+
+	s.GlyphStyle.Shape = draw.CrossGlyph{}
+	s.Color = color.RGBA{R: 255, A: 255}
+	p.Add(s)
+
+	po.GlyphStyle.Shape = draw.CrossGlyph{}
+	po.Color = color.RGBA{R: 0, A: 255}
+	p.Add(po)
+
+	wt, err := p.WriterTo(256, 256, "png")
+	if err != nil {
+		return fmt.Errorf("could not create writer: %v", err)
+	}
+	_, err = wt.WriteTo(f)
+	if err != nil {
+		return fmt.Errorf("could not write to %s: %v", path, err)
+	}
+
+	if err := f.Close(); err != nil {
+		return fmt.Errorf("could not close %s: %v", path, err)
+	}
+	return nil
+}
