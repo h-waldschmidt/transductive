e99f6e9f01158b849637858bf797fe0c65f1c38d
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-07-11 21:19:43 +0200

    tests slice addition (added wrong file previously)

--- cmd/kmeans/kmeans_test.go
+++ cmd/kmeans/kmeans_test.go
@@ -1,1 +1,34 @@
 package kmeans
+
+import (
+	"testing"
+)
+
+func TestSliceAdditionNormal(t *testing.T) {
+	a := []float64{1, 2, 3, 4, 5}
+	b := []float64{1, 2, 3, 4, 5}
+	ans, err := sliceAddition(a, b)
+
+	if len(ans) != 5 {
+		t.Error("answer has unexpected length")
+	}
+
+	if err != nil {
+		t.Errorf("didn't expect error: %v", err)
+	}
+
+	for i := 0; i < 5; i++ {
+		if ans[i] != a[i]+b[i] {
+			t.Error("unexpected value")
+		}
+	}
+}
+
+func TestSliceAdditionError(t *testing.T) {
+	a := []float64{1, 2, 3, 4, 5}
+	b := []float64{1, 2, 3, 4}
+	ans, err := sliceAddition(a, b)
+	if ans != nil || err == nil {
+		t.Error("Expected error")
+	}
+}
