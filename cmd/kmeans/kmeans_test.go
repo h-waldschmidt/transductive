8f49e15a6ded28a182a3bb6d3c06e6f4d2b61e0c
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-07-11 21:29:48 +0200

    tests for sliceMult

--- cmd/kmeans/kmeans_test.go
+++ cmd/kmeans/kmeans_test.go
@@ -2,6 +2,8 @@ package kmeans
 
 import (
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestSliceAdditionNormal(t *testing.T) {
@@ -17,10 +19,10 @@ func TestSliceAdditionNormal(t *testing.T) {
 		t.Errorf("didn't expect error: %v", err)
 	}
 
-	for i := 0; i < 5; i++ {
-		if ans[i] != a[i]+b[i] {
-			t.Error("unexpected value")
-		}
+	expected := []float64{2, 4, 6, 8, 10}
+
+	if !cmp.Equal(expected, ans) {
+		t.Error("unexpected value")
 	}
 }
 
@@ -28,7 +30,39 @@ func TestSliceAdditionError(t *testing.T) {
 	a := []float64{1, 2, 3, 4, 5}
 	b := []float64{1, 2, 3, 4}
 	ans, err := sliceAddition(a, b)
+
 	if ans != nil || err == nil {
 		t.Error("Expected error")
 	}
+}
+
+func TestSliceMultiplicationNormal(t *testing.T) {
+	a := []float64{1, 2, 3, 4, 5}
+
+	t.Run("Zero Slice", func(t *testing.T) {
+		ans := sliceMultiplication(a, 0)
+		expected := make([]float64, 5)
+
+		if !cmp.Equal(expected, ans) {
+			t.Error("expected zero slice")
+		}
+	})
+
+	t.Run("Same", func(t *testing.T) {
+		ans := sliceMultiplication(a, 1)
+		expected := []float64{1, 2, 3, 4, 5}
+
+		if !cmp.Equal(expected, ans) {
+			t.Error("expected same slice")
+		}
+	})
+
+	t.Run("Normal", func(t *testing.T) {
+		ans := sliceMultiplication(a, 2)
+		expected := []float64{2, 4, 6, 8, 10}
+
+		if !cmp.Equal(expected, ans) {
+			t.Error("unexpected value")
+		}
+	})
 }
