c18f689ced299759962dc50b0ca3ba2bc411e551
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-07-21 16:13:28 +0200

    conversion and generator tests

--- cmd/plt/conversion_test.go
+++ cmd/plt/conversion_test.go
@@ -1,1 +1,41 @@
 package plt
+
+import (
+	"testing"
+	"transductive-experimental-design/cmd/lialg"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestConvertSliceToCoordinateNormal(t *testing.T) {
+	point := []float64{1, 1}
+
+	result := ConvertSliceToCoordinate(point)
+	expected := Coordinate{1, 1}
+
+	if !cmp.Equal(result, expected) {
+		t.Errorf("expected: %v, got: %v", expected, result)
+	}
+}
+
+func TestConvertMatrixToCoordinateSliceNormal(t *testing.T) {
+	matrix := lialg.Matrix{N: 3, M: 2, Matrix: [][]float64{{3, 1}, {2, 0}, {1, 2}}}
+
+	result := ConvertMatrixToCoordinateSlice(&matrix)
+	expected := []Coordinate{{3, 1}, {2, 0}, {1, 2}}
+
+	if !cmp.Equal(result, expected) {
+		t.Errorf("expected: %v, got: %v", expected, result)
+	}
+}
+
+func TestConvertCoordinatesToMatrixNormal(t *testing.T) {
+	input := []Coordinate{{3, 1}, {2, 0}, {1, 2}}
+
+	result := ConvertCoordinatesToMatrix(input)
+	expected := lialg.Matrix{N: 3, M: 2, Matrix: [][]float64{{3, 1}, {2, 0}, {1, 2}}}
+
+	if !cmp.Equal(result, expected) {
+		t.Errorf("expected: %v, got: %v", expected, result)
+	}
+}
