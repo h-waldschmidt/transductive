4c33819e7e69877f75451bd53c25552ab306504c
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-08-09 20:51:24 +0200

    added sillhouette coefficient for evaluate

--- cmd/kmeans/kmeans.go
+++ cmd/kmeans/kmeans.go
@@ -8,8 +8,6 @@ import (
 	"sync"
 	"time"
 	"transductive-experimental-design/cmd/lialg"
-
-	"golang.org/x/exp/constraints"
 )
 
 // Clusters saves all the central points (centroids) as a matrix
@@ -17,15 +15,23 @@ import (
 // index of point in points matrix represents index in assignments slice
 // assignments slice saves the index of the cluster it is assigned to
 type Clusters struct {
-	Points      *lialg.Matrix
-	Centroids   lialg.Matrix
-	Assignments []int
+	Points        *lialg.Matrix
+	NumOfClusters int
+	ClusterPoints [][]int
+	Centroids     lialg.Matrix
+	Assignments   []int
 }
 
 // cluster the data by using the basic k means clustering algorithm
 // the centroids in the first rounds are randomly initialized
 func Calculate(points lialg.Matrix, numOfClusters int) (Clusters, error) {
-	clusters := Clusters{&points, *lialg.NewMatrix(numOfClusters, points.M), make([]int, points.N)}
+	clusters := Clusters{
+		&points,
+		numOfClusters,
+		make([][]int, numOfClusters),
+		*lialg.NewMatrix(numOfClusters, points.M),
+		make([]int, points.N),
+	}
 
 	if points.N < numOfClusters {
 		return clusters, fmt.Errorf("there should be at least %d points", numOfClusters)
@@ -47,7 +53,7 @@ func Calculate(points lialg.Matrix, numOfClusters int) (Clusters, error) {
 
 			d := math.Inf(1)
 			for _, centroid := range clusters.Centroids.Matrix {
-				d = min(d, lialg.EuclideanDistance(point, centroid))
+				d = math.Min(d, lialg.EuclideanDistance(point, centroid))
 			}
 			distances[j] = d
 		}
@@ -65,7 +71,10 @@ func Calculate(points lialg.Matrix, numOfClusters int) (Clusters, error) {
 				minDistance := math.Inf(1)
 				bestClusterIndex := -1
 				for k := 0; k < clusters.Centroids.N; k++ {
-					distance := lialg.EuclideanDistance(points.Matrix[j], clusters.Centroids.Matrix[k])
+					distance := lialg.EuclideanDistance(
+						points.Matrix[j],
+						clusters.Centroids.Matrix[k],
+					)
 
 					if distance < minDistance {
 						minDistance = distance
@@ -76,7 +85,7 @@ func Calculate(points lialg.Matrix, numOfClusters int) (Clusters, error) {
 			}(j)
 		}
 		wg.Wait()
-		clusters.updateCentroids()
+		clusters.updateClusters()
 	}
 	return clusters, nil
 }
@@ -92,18 +101,72 @@ func (clusters *Clusters) Inertia() float64 {
 	return inertia
 }
 
+// calculates the silhouette coefficient of the cluster
+// see: https://en.wikipedia.org/wiki/Silhouette_(clustering)
+func (clusters *Clusters) SilhouetteCoefficient() float64 {
+	var coefficient float64
+	for i := range clusters.Points.Matrix {
+		if i == 10 {
+			fmt.Printf("")
+		}
+		coefficient += clusters.silhouette(i)
+	}
+	return coefficient / float64(len(clusters.Points.Matrix))
+}
+
+func (clusters *Clusters) silhouette(index int) float64 {
+	var intraDistance float64
+	clusterIndex := clusters.Assignments[index]
+	if len(clusters.ClusterPoints[clusterIndex]) > 1 {
+		for _, val := range clusters.ClusterPoints[clusterIndex] {
+
+			intraDistance += lialg.EuclideanDistance(
+				clusters.Points.Matrix[val],
+				clusters.Points.Matrix[index],
+			)
+		}
+		intraDistance /= float64(len(clusters.ClusterPoints[clusterIndex]) - 1)
+	}
+
+	var nearestClusterDistance float64
+	for _, val := range clusters.ClusterPoints[clusterIndex] {
+		d := math.Inf(1)
+		for i := range clusters.Centroids.Matrix {
+			if i != clusterIndex {
+				var cache float64
+				for _, ind := range clusters.ClusterPoints[i] {
+					cache += lialg.EuclideanDistance(clusters.Points.Matrix[val], clusters.Points.Matrix[ind])
+				}
+				cache /= float64(len(clusters.ClusterPoints[i]))
+				d = math.Min(d, cache)
+			}
+		}
+		nearestClusterDistance += d
+	}
+	nearestClusterDistance /= float64(len(clusters.Centroids.Matrix) - 1)
+	silhouette := (nearestClusterDistance - intraDistance) / math.Max(nearestClusterDistance, intraDistance)
+	return silhouette
+}
+
 // helper function that updates the centroids by calculating
 // the average of the items in the cluster
-func (clusters *Clusters) updateCentroids() {
+func (clusters *Clusters) updateClusters() {
 	clusters.Centroids = *lialg.NewMatrix(clusters.Centroids.N, clusters.Centroids.M)
 
 	var err error
 	clusterNumOfItems := make([]int, clusters.Centroids.N)
 
+	// reset cluster points
+	clusters.ClusterPoints = make([][]int, clusters.NumOfClusters)
+
 	for i, cluster := range clusters.Assignments {
 		clusterNumOfItems[cluster]++
 
-		clusters.Centroids.Matrix[cluster], err = sliceAddition(clusters.Centroids.Matrix[cluster], clusters.Points.Matrix[i])
+		clusters.ClusterPoints[cluster] = append(clusters.ClusterPoints[cluster], i)
+
+		clusters.Centroids.Matrix[cluster], err = sliceAddition(
+			clusters.Centroids.Matrix[cluster],
+			clusters.Points.Matrix[i])
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -111,7 +174,10 @@ func (clusters *Clusters) updateCentroids() {
 
 	for i := 0; i < clusters.Centroids.N; i++ {
 		if clusterNumOfItems[i] != 0 {
-			clusters.Centroids.Matrix[i] = sliceMultiplication(clusters.Centroids.Matrix[i], 1/float64(clusterNumOfItems[i]))
+			clusters.Centroids.Matrix[i] = sliceMultiplication(
+				clusters.Centroids.Matrix[i],
+				1/float64(clusterNumOfItems[i]),
+			)
 		}
 	}
 }
@@ -138,22 +204,6 @@ func sliceMultiplication(a []float64, factor float64) []float64 {
 		ans[i] = factor * a[i]
 	}
 	return ans
-}
-
-// return smaller value of two given values
-func min[T constraints.Ordered](a, b T) T {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-// return larger value of two given values
-func max[T constraints.Ordered](a, b T) T {
-	if a > b {
-		return a
-	}
-	return b
 }
 
 func maxIndexSlice(a []float64) int {
